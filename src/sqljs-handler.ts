b63e481776acce1d8e00234478160d1c82e2066c
citycide <lingenbw@gmail.com>
2021-01-29 23:47:39 -0600

    rework structure, name things better, and improve inference
    Rework code structure. Rename many types for clarity (e.g. `cast.toDefinition` is now `cast.serialize`).
    
    * `cast.serialize` has issues with criteria types
    * `ColumnDescriptor` static typing isn't working properly, especially for `get` & `set`

--- src/sqljs-handler.ts
+++ src/sqljs-handler.ts
@@ -1,11 +1,37 @@
 import { dirname } from "path"
 import { mkdirSync, readFileSync, writeFileSync } from "fs"
 
+import { SqlJs } from "sql.js/module"
 import * as pool from "generic-pool"
 
 import { Trilogy } from "."
+import { LooseObject } from "./types/utils"
 
-import { SqlJs } from "sql.js/module"
+export type SqlJsResponse = Array<{
+  columns: string[]
+  values: unknown[][]
+}>
+
+export const parseResponse = (
+  contents: SqlJsResponse
+): LooseObject[] => {
+  if (contents?.length < 1) return []
+
+  const [{ columns, values }] = contents
+  const results = []
+
+  for (let i = 0; i < values.length; i++) {
+    const line: LooseObject = {}
+
+    for (let j = 0; j < columns.length; j++) {
+      line[columns[j]] = values[i][j]
+    }
+
+    results.push(line)
+  }
+
+  return results
+}
 
 export async function readDatabase (instance: Trilogy): Promise<SqlJs.Database> {
   const name = instance.options.connection.filename
